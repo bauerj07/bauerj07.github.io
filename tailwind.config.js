adaef159723efcd2ba34ff4fdc41d2a176b726bf
Josh Bauer <josh.bauer@relode.com>
2024-04-20 21:35:09 -0400

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** this function takes in a css color variable name as well as extract a tailwind opacity class that may be present to combined the two
+  link below helps explain this
+  * https://www.youtube.com/watch?v=MAtaT8BZEAo&t=891s **/
+  function withOpacity(variableName) {
+    return ({ opacityValue }) => {
+        if (opacityValue != undefined) {
+            return `rgba(var(${variableName}), ${opacityValue})`;
+        }
+        return `rgb(var(${variableName})`;
+    };
+}
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
